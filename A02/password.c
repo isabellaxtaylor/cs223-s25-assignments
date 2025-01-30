0e05602d00e89de1fff2028723a581c36746ccc6
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-01-30 15:39:06 -0500

    done

--- A02/password.c
+++ A02/password.c
@@ -1,9 +1,43 @@
 /***************************************************
- * Author: 
+ * Author: Isabella taylor
+ * Program: Bad Password Generator
+ * Description: This program takes a word as input and generates a "bad" password
+ * by replacing specific characters:
+ *  - 'e' with '3'
+ *  - 'i' with '1'
+ *  - 'a' with '@'
  * 
+ * The program then outputs the transformed password.
  */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  return 0;
+    char pass[100];
+    char badPass[100] = {0};
+
+    printf("Enter a word: ");
+    scanf("%s", pass);
+
+    int n = strlen(pass);
+
+    for (int i = 0; i < n; i++) {  
+        if (pass[i] == 'e') {
+            badPass[i] = '3';  
+        } 
+        else if (pass[i] == 'i') {
+            badPass[i] = '1';  
+        } 
+        else if (pass[i] == 'a') {
+            badPass[i] = '@';  
+        } 
+        else {
+            badPass[i] = pass[i];  
+        }
+    }
+    
+    badPass[n] = '\0';
+    printf("Your bad password is %s\n", badPass);  
+
+    return 0;
 }
