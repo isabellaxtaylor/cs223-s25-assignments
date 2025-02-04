5ceb26c8bcbf6e1020642437013a00a0d9e89eb2
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-02-04 15:22:00 -0500

    done

--- J01/countsort.c
+++ J01/countsort.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-   return 0;
+void sort_digits(char *digits) {
+    char temp;
+    int n = strlen(digits);
+    
+    for (int i = 0; i < n; i++) {
+        for (int x = i + 1; x < n; x++) {
+            if (digits[i] > digits[x]) {
+                temp = digits[i];
+                digits[i] = digits[x];
+                digits[x] = temp;
+            }
+        }
+    }
+    printf("%s\n", digits);
+}
+
+int main() {
+    char input[100];
+    printf("Enter digits: ");
+    scanf("%s", input);
+    sort_digits(input);
+    return 0;
 }
