acd443b356d4abe8d2db98556ce8951a45420746
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-02-04 15:49:37 -0500

    done

--- J01/price.c
+++ J01/price.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-   return 0;
+int winner(int a, int b, int c, int d) {
+    int diff_a = (a <= d) ? d - a : -1;
+    int diff_b = (b <= d) ? d - b : -1;
+    int diff_c = (c <= d) ? d - c : -1;
+
+    if (diff_a >= 0 && (diff_b == -1 || diff_a < diff_b) && (diff_c == -1 || diff_a < diff_c)) {
+        return a;
+    } else if (diff_b >= 0 && (diff_c == -1 || diff_b < diff_c)) {
+        return b;
+    } else if (diff_c >= 0) {
+        return c;
+    }
+    return -1;
 }
 
+int main() {
+    srand(time(0));
+
+    int guess, guess1, guess2, cost;
+
+    printf("Welcome to The Price is Right!\n");
+    printf("Enter a guess to win a luxurious dishwasher: $");
+    scanf("%d", &guess);
+
+    guess1 = 3000 + rand() % (5000 - 3000 + 1);
+    guess2 = 3000 + rand() % (5000 - 3000 + 1);
+    cost = 3000 + rand() % (5000 - 3000 + 1);
+
+    printf("AI contestant #1 guesses: $%d\n", guess1);
+    printf("AI contestant #2 guesses: $%d\n", guess2);
+    printf("The dishwasher cost: $%d\n", cost);
+
+    int winning_guess = winner(guess, guess1, guess2, cost);
+
+    if (winning_guess == guess) {
+        printf("Congratulations! You won!\n");
+    } else if (winning_guess == guess1) {
+        printf("AI contestant #1 wins!\n");
+    } else if (winning_guess == guess2) {
+        printf("AI contestant #2 wins!\n");
+    } else {
+        printf("No one won! Everyone overbid.\n");
+    }
+
+    return 0;
+}
