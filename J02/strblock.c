45689f284a98f3c2613082ad08dd073f3bad77ff
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-02-18 15:21:28 -0500

    done

--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main()
 {
-  return 0;
+    char word[31];
+    printf("Enter a string: ");
+    scanf("%s",word);
+    
+    int length=strlen(word);
+    char* array = malloc(length * length * sizeof(char));
+    memset(array,' ',length*length);
+    
+    for (int i=0;i<length;i++) {
+        array[i]=word[i];
+        array[(length-1)*length+i]=word[i];
+        array[i*length+i]=word[i];
+    }
+    for (int i=0;i<length;i++) {
+        for (int j=0;j<length;j++) {
+        printf("%c",array[i*length+j]);
+        }
+      printf("\n");
+    }
+    free(array);
+    return 0;
 }
