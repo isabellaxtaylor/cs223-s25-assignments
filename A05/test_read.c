2c31bb93950f5eb6db7b43c345a1776ec7e41fa9
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-02-21 08:42:33 -0500

    done

--- A05/test_read.c
+++ A05/test_read.c
@@ -1,18 +1,36 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Isabella taylor
+ * Date: 2/21/2025
+ * Description: Reads and prints pixel data from a PPM file.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_ppm.h"
 
 int main() {
-  int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+    int w, h;
 
-  // todo: print out the grid of pixels
-  free(pixels);
-  return 0;
+    // Read the PPM file
+    struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+    if (!pixels) {
+        fprintf(stderr, "Error: Failed to read PPM file\n");
+        return 1;
+    }
+
+    // Print the width and height
+    printf("Testing file feep-raw.ppm: %d %d\n", w, h);
+
+    // Print the pixel values
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            struct ppm_pixel pixel = pixels[i * w + j];
+            printf("(%d,%d,%d) ", pixel.red, pixel.green, pixel.blue);
+        }
+        printf("\n");
+    }
+
+    free(pixels);
+    return 0;
 }
+
 
