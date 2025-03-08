86255bf9672db8d2b84671fdccdbf4397c7d8353
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-03-07 19:37:17 -0500

    done

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -1,15 +1,27 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Isabella Taylor
+ * Date: 3/7/2025
+ * Description: Converts 64-bit hex to 8x8 bitmap.
  ---------------------------------------------*/
 #include <stdio.h>
 
 int main() {
-  unsigned long img;
-  scanf(" %lx", &img);
-  printf("Image (unsigned long): %lx\n", img);
+    unsigned long img;
+    scanf(" %lx", &img);
+    printf("Image (unsigned long): %lx\n", img);
 
-  // todo: your code here
-  return 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            unsigned long mask = 0x1ul << (63 - (i * 8 + j));
+
+            if (img & mask) {
+                printf("@");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
