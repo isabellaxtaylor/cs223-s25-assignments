01fe4fab72dacf8e50d8544df22f01cc875957af
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-03-07 21:00:38 -0500

    done

--- A07/write_ppm.c
+++ A07/write_ppm.c
@@ -1,18 +1,27 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Isabella Taylor
+ * Date: 3/7/2025
+ * Description: Writes pixel data to a PPM file in binary format.
  ---------------------------------------------*/
-#include "write_ppm.h"
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include "write_ppm.h"
 
-// implement *one* (but do not remove the other one)!
+// Writes a PPM file in binary format
+void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+    FILE* fp = fopen(filename, "wb");
+    if (!fp) {
+        fprintf(stderr, "Error: Cannot open file %s for writing\n", filename);
+        return;
+    }
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+    // Write the PPM header
+    fprintf(fp, "P6\n%d %d\n255\n", w, h);
 
-}
+    // Write the pixel data
+    if (fwrite(pxs, sizeof(struct ppm_pixel), w * h, fp) != (size_t)(w * h)) {
+        fprintf(stderr, "Error: Failed to write pixel data\n");
+    }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
-
+    fclose(fp);
 }
