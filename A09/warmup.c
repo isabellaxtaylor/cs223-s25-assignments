9f94b269bdecdb4f9c47d955c0a34c70d7917055
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-03-28 12:01:08 -0400

    done

--- A09/warmup.c
+++ A09/warmup.c
@@ -1,5 +1,32 @@
+/*----------------------------------------------
+ * Author: Isabella Taylor
+ * Date: 3/28/2025
+ * Description: Forked Process Spawning and PID Output.
+ ---------------------------------------------*/
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-  return 0;
+    pid_t ret;
+
+    printf("%d] A0\n", getpid());
+
+    ret = fork();
+
+    if (ret == 0) {
+        printf("%d] B1\n", getpid());
+    } else {
+        printf("%d] B0\n", getpid());
+
+        ret = fork();
+        if (ret == 0) {
+            printf("%d] C1\n", getpid());
+        } else {
+            printf("%d] C0\n", getpid());
+        }
+    }
+
+    printf("%d] Bye\n", getpid());
+    return 0;
 }
