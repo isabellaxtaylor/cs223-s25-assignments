46edba0d2b019499a8c9c4669d37d6d7909b174b
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-04-22 17:11:17 -0400

    done

--- A13/mylloc_list.c
+++ A13/mylloc_list.c
@@ -3,18 +3,47 @@
 #include <unistd.h>
 
 struct chunk {
-  int size;
-  int used;
-  struct chunk *next;
+    int size;      
+    int used;      
+    struct chunk *next;
 };
 struct chunk *flist = NULL;
 
-void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+void *malloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
+
+    struct chunk **prevp = &flist;
+    struct chunk  *curr = flist;
+    while (curr) {
+        if (curr->size >= (int)size) {
+            *prevp = curr->next;
+            curr->used = size;
+            curr->next = NULL;
+            return (void*)(curr + 1);
+        }
+        prevp = &curr->next;
+        curr  = curr->next;
+    }
+
+    size_t total = sizeof(struct chunk) + size;
+    struct chunk *hdr = sbrk(total);
+    if (!hdr) {
+        return NULL;
+    }
+    hdr->size = size;
+    hdr->used = size;
+    hdr->next = NULL;
+    return (void*)(hdr + 1);
 }
 
 void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return;
+    if (memory == NULL) {
+        return;
+    }
+    struct chunk *hdr = (struct chunk*)memory - 1;
+    hdr->used = 0;
+    hdr->next = flist;
+    flist  = hdr;
 }
