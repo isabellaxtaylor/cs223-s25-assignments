6ed0e9f65045baafa2320188189bcb013c6422d9
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-04-22 15:33:32 -0400

    done

--- J06/shot.c
+++ J06/shot.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
-{
-  return 0;
+void handle_sigint(int sig) {
+    printf("Help! I've been shot!\n");
+    fflush(stdout);
+    exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+    if (signal(SIGINT, handle_sigint) == SIG_ERR) {
+        perror("signal");
+        return EXIT_FAILURE;
+    }
+
+
+    while (1) {
+        pause();
+    }
+
+    return EXIT_SUCCESS;
 }
