d535cebb70cb81d4fee3750cce0cae90a1930179
Isabella Lyon Aridi Taylor <itaylor@goldengate.cs.brynmawr.edu>
2025-04-22 15:26:57 -0400

    done

--- J06/decipher.c
+++ J06/decipher.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    FILE* fp = fopen("message", "r");
-    char c = fgetc(fp);
-    while (c != EOF)
-    {
-        printf("%c", c);
-        c = fgetc(fp);
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    int  c;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    return 0;
+
+    fp = fopen(argv[1], "r");
+    if (!fp) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        unsigned char usignc = (unsigned char)c;
+        usignc = (usignc >> 1) & 0x7F;
+        putchar(usignc);
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
 }
